0a9ac4de5c7c0758693735bb9cb3d6460ed61bad
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-03-21 23:02:51 +0530

    sixth

--- src/Component/Expense/ExpenseDetails.js
+++ src/Component/Expense/ExpenseDetails.js
@@ -1,12 +1,16 @@
 import "./Expenseitem.css";
 
 const ExpenseDetails = (props) => {
-
+  const clickHandler =(e)=>{
+    let d = document.activeElement.parentElement.parentElement
+    d.remove()
+  }
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <p>{`Location - ${props.location}`}</p>
       <div className="expense-item__price">{`₹ ${props.amount}`}</div>
+      <button className="expense-item__delete" onClick={clickHandler}>Delete</button>
     </div>
   );
 };
